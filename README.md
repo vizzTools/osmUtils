3b2553608aebf110487f2aedcc5a05e29526ad80
elpamart <elena.palao@vizzuality.com>
2020-10-22 15:48:07 +0200

    Initial commit library setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Open Street Map Interface
+
+Interfaces with overpass API and retrieved Open Street Map (OSM) data.
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+import setuptools
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setuptools.setup(
+    name="osmUtils",
+    version="0.0.1",
+    description="Provides library functions to interface overpass API",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vizzTools/osmUtils",
+    author="Elena Palao",
+    author_email="elena.palao@vizzuality.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    packages=['osmUtils],
+    install_requires=['requests>=2.2.0', 'folium==0.8.3'],
+    entry_points={
+        "console_scripts": [
+            "vizzpython=osmUtils.__main__:main",
+        ]
+    },
+)
